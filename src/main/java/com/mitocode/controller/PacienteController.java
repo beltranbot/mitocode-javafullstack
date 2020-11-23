7a811de1428fee48a99625012f510c69b9735ff8
beltranbot <beltranbot@gmail.com>
2020-11-22 23:16:38 -0500

    added backend pagination for paciente/pageable endpoint

--- src/main/java/com/mitocode/controller/PacienteController.java
+++ src/main/java/com/mitocode/controller/PacienteController.java
@@ -9,6 +9,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
@@ -86,6 +88,12 @@ public class PacienteController {
 
 		service.eliminar(id);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/pageable")
+	public ResponseEntity<Page<Paciente>> listarPageable(Pageable pageable) throws Exception{
+		Page<Paciente> pacientes = service.listarPageable(pageable);
+		return new ResponseEntity<Page<Paciente>>(pacientes, HttpStatus.OK);
 	}
 
 }
